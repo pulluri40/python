d3bd6b374d4c58ef36142876d51b1b19300b599f
padmini <pulluri40@gmail.com>
2023-12-07 11:42:33 +0530

    adding 2 num

---
+++ sum of 2 num.py
@@ -0,0 +1,12 @@
+'''
+num1=12
+num2=14
+add=num1+num2
+print("The sum of two numbers is :",add)
+'''
+
+def add(num1,num2):
+    sum=num1+num2
+    return sum
+result=add(12,14)
+print("The sum of two numbers: ",result)
